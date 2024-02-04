9dc1c0a34fec3dce75cc99464fb9cc06d23672b6
Abel-mwirigi <mwirigiabel223359@daystar.ac.ke>
2024-02-04 18:08:32 +0300

    post method to send data to the backend

--- src/login.jsx
+++ src/login.jsx
@@ -10,8 +10,39 @@ import {
   Container,
   Button,
 } from "@chakra-ui/react";
+import { useState } from "react";
+
 
 export default function Login() {
+    const [inputs, setInputs] = useState({
+      email: "",
+      username: "",
+      password: "",
+    });
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}));
+    }
+
+    const handleCreateUser = () => {
+        const requestOptions = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(inputs),
+        };
+        fetch("http://127.0.0.1:8000/create-user", requestOptions)
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+            return response.json();
+          })
+          .then((data) => console.log(data))
+          .catch((error) => console.error("Error during fetch:", error));
+    }
+
   return (
     <ChakraProvider theme={theme}>
       <Container>
@@ -26,12 +57,29 @@ export default function Login() {
             </Link>
           </Text>
           <VStack spacing={2} align="stretch" pb={3}>
-            <input placeholder="Username" />
-            <input placeholder="Email" />
-            <input placeholder="Password" />
-            <input placeholder="Confirm password" />
+            <input
+              type="email"
+              name="email"
+              value={inputs.email}
+              placeholder="Email"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="username"
+              value={inputs.username}
+              placeholder="Username"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              name="password"
+              value={inputs.password}
+              placeholder="Password"
+              onChange={handleChange}
+            />
           </VStack>
-          <Button colorScheme="teal" variant="solid" isFullWidth="true">
+          <Button colorScheme="teal" variant="solid" onClick={handleCreateUser}>
             Create Account
           </Button>
         </Box>
